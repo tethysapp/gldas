37cce508ef220afc85d2c900da185883c37fc72f
Rohit Khattar <rohitkh@byu.edu>
2020-07-09 18:52:02 +0000

    Warehouse_Commit

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_resource_files
 
 # -- Apps Definition -- #
 app_package = 'gldas'
